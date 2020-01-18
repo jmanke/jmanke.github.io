8a052814484a7e990d75db07af3248cdfd6da3f7
Jeff Manke <jeffman879@gmail.com>
2020-01-17 19:47:23 -0800

    Updates

---
+++ precache-manifest.9db92d47967690d495f411d174b4a67d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5433d810572f53f12e451332a142c682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1399edab11a406dafc25",
+    "url": "/static/js/2.29e955fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29e955fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c73b931b3cbea36f4e4d",
+    "url": "/static/js/main.6c819896.chunk.js"
+  },
+  {
+    "revision": "d1a19a062f5214fe4145",
+    "url": "/static/js/runtime-main.ebe23bff.js"
+  },
+  {
+    "revision": "f83b4400ec9703385d039d5c023cb6c7",
+    "url": "/static/media/logo.f83b4400.png"
+  },
+  {
+    "revision": "ba6397f199b7992fce05aec5e18c05c6",
+    "url": "/static/media/tft.ba6397f1.png"
+  }
+]);
