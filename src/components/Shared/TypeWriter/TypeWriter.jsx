f1dda22f94dce00974a175db35645867aae8432b
jmanke <jeffman879@gmail.com>
2020-01-24 18:22:47 -0800

    fixed type-writer bug with re-renders

--- src/components/Shared/TypeWriter/TypeWriter.jsx
+++ src/components/Shared/TypeWriter/TypeWriter.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./TypeWriter.css";
 
 export default props => {
+  const waitingOnUpdate = React.useRef(false);
   const [currTextIndex, setCurrTextIndex] = React.useState(0);
   const minDelay = 0.1;
 	const maxDelay = 0.2;
 	
-	const textCompelte = props.text.length === currTextIndex;
+	const textComplete = props.text.length === currTextIndex;
 
-  if (!textCompelte) {
+  if (!textComplete && !waitingOnUpdate.current) {
+    waitingOnUpdate.current = true;
+
     setTimeout(() => {
+      waitingOnUpdate.current = false;
       setCurrTextIndex(currTextIndex + 1);
     }, (Math.random() * (maxDelay - minDelay) + minDelay) * 1000);
   }
@@ -17,7 +21,7 @@ export default props => {
   return (
     <p className={"type-writer"}>
       {props.text.slice(0, currTextIndex)}
-      <span className={textCompelte ? "type-line blink" : "type-line"}>{"|"}</span>
+      <span className={textComplete ? "type-line blink" : "type-line"}>{"|"}</span>
     </p>
   );
 };
