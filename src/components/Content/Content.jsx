a318302a2766f5e62f09bc6c12aba3bf3a279fb4
jmanke <jeffman879@gmail.com>
2020-01-29 12:07:11 -0800

    css for timeline fixes

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -11,10 +11,10 @@ import Contact from "./Sections/Contact";
 export default () => (
   <div className="content">
     <LandingPage />
-    <Section children={<About />} className={"section-about"} title={"About"} bgColor={"bg-primary"} />
-    <Section children={<Experience />} className={"section-experience"} title={"Experience"} bgColor={"bg-secondary"} />
-    <Section children={<Projects />} className={"section-projects"} title={"Projects"} bgColor={"bg-primary"} />
-    <Section children={<Education />} className={"section-education"} title={"Education"} bgColor={"bg-secondary"} />
-    <Section children={<Contact />} className={"section-contact"} title={"Contact"} bgColor={"bg-dark"} />
+    <Section children={<About />} className={"about"} title={"About"} bgColor={"bg-primary"} />
+    <Section children={<Experience />} className={"experience"} title={"Experience"} bgColor={"bg-secondary"} />
+    <Section children={<Projects />} className={"projects"} title={"Projects"} bgColor={"bg-primary"} />
+    <Section children={<Education />} className={"education"} title={"Education"} bgColor={"bg-secondary"} />
+    <Section children={<Contact />} className={"contact"} title={"Contact"} bgColor={"bg-dark"} />
   </div>
 );
