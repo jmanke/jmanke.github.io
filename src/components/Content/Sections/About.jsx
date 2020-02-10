6021f654e73289469fe643a6726916153bedc254
jmanke <jeffman879@gmail.com>
2020-02-10 15:50:57 -0800

    styled-components refactor wip

--- src/components/Content/Sections/About.jsx
+++ src/components/Content/Sections/About.jsx
@@ -1,23 +1,71 @@
 import React from "react";
-import FadeSection from "../../Shared/FadeSection/FadeSection";
-import InfoCard from "../../Shared/InfoCard/InfoCard";
-import developmentImg from "../../../images/monitor.svg";
-import manImg from "../../../images/man.svg";
-import hobbyImg from "../../../images/coin.svg";
+import styled from "styled-components";
+import FadeSection from "components/Shared/FadeSection/FadeSection";
+import InfoCard from "components/Shared/InfoCard/InfoCard";
+import developmentImg from "images/monitor.svg";
+import manImg from "images/man.svg";
+import hobbyImg from "images/coin.svg";
+import styleVars from "styleVars";
+
+const InfoCardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: ${styleVars.tabletWidth}) {
+    flex-wrap: wrap;
+    
+    .info-card__development{
+      order: 2;
+      margin-right: auto;
+      width: 45%;
+    }
+
+    .info-card__about-me {
+      order: 1;
+      width: 100%;
+    }
+
+    .info-card__hobbies {
+      order: 3;
+      margin-left: auto;
+      width: 45%;
+    }
+  }
+
+  @media screen and (max-width: ${styleVars.mobileWidth}) {
+    flex-wrap: wrap;
+    
+    .info-card__development{
+      margin-right: initial;
+      width: 100%;
+    }
+
+    .info-card__about-me {
+      width: 100%;
+    }
+
+    .info-card__hobbies {
+      margin-left: initial;
+      width: 100%;
+    }
+  }
+`
 
 export default () => {
   return (
     <div className="about unselectable">
       <FadeSection>
-        <div className="about__info">
+        <InfoCardsContainer>
           <InfoCard
+            className="info-card__development"
             title={"Development"}
             secondary={true}
             icon={developmentImg}
             text={
               "Whether its writing efficient, multi-threaded \
-            C# or C++ code, or front-end with JavaScript and React, \
-            I love taking on new challenges and solving problems."
+              C# or C++ code, or front-end with JavaScript and React, \
+              I love taking on new challenges and solving problems."
             }
           />
           <InfoCard
@@ -32,8 +80,10 @@ export default () => {
             spend most of my time programming and learning new \
             technologies."
             }
+            primary
           />
           <InfoCard
+            className="info-card__hobbies"
             title={"Hobbies"}
             secondary={true}
             icon={hobbyImg}
@@ -42,7 +92,7 @@ export default () => {
         games are Kerbal Space Program, Halo, The Witcher 3, and The Outer Wilds."
             }
           />
-        </div>
+        </InfoCardsContainer>
       </FadeSection>
     </div>
   );
--- src/components/Shared/InfoCard/InfoCard.jsx
+++ src/components/Shared/InfoCard/InfoCard.jsx
@@ -1,20 +1,80 @@
 import React from "react";
-import "./InfoCard.css";
+import styled from "styled-components";
+import styleVars from "styleVars";
+
+const InfoCard = styled.div`
+  width: ${props => props.primary ? `22em` : `18em`};
+  height: ${props => props.primary ? '26.84em' : `21.96em`};
+  background-color: white;
+  box-shadow: 0 0 7px rgb(175, 207, 228);
+  display: flex;
+  flex-direction: column;
+  transition: all 0.2s ease-in-out;
+  margin: 1em;
+
+  &:hover {
+    box-shadow: 0 0 10px rgb(175, 207, 228);
+  }
+
+  @media screen and (max-width: ${styleVars.mobileWidth}) {
+    height: fit-content;
+  }
+`
+
+const IconContainer = styled.div`
+  width: 100%;
+  height: ${props => props.primary ? `12em` : `10em`};
+  display: flex;
+`
+
+const Icon = styled.img`
+  height: 60%;
+  margin: auto;
+`
+
+const Title = styled.h3`
+  padding: 0.5rem 0;
+  width: 100%;
+  text-align: center;
+  color: ${props => props.primary ? `#FFFFFF` : `#1C6FA2`};
+  background-color: ${props => props.primary ? `#1C6FA2` : `#FFFFFF`};
+  margin-top: 0;
+  margin-bottom: 0rem;
+`
+
+const TextContainer = styled.div`
+  width: 100%;
+  height: 55%;
+  background-color: ${props => props.primary ? `#FFFFFF` : `#1C6FA2`};
+  display: flex;
+
+  @media screen and (max-width: ${styleVars.mobileWidth}) {
+    height: fit-content;
+  }
+`
+
+const Description = styled.p`
+  margin: auto;
+  text-align: center;
+  padding: 5%;
+  height: 85%;
+  color: ${props => props.primary ? `#1C6FA2` : `#FFFFFF`};
+`
 
 export default props => {
   return (
-    <div className={"info-card " + (props.secondary ? "info-card_secondary " : "") + (props.className ?? "")}>
-      <div className="info-card__icon-container">
-        <img src={props.icon} className="info-card__icon" alt={props.iconAlt ?? "icon"} />
-      </div>
-      <h3 className="info-card__title txt-lg">
+    <InfoCard className={props.className ?? ""} primary={props.primary}>
+      <IconContainer primary={props.primary}>
+        <Icon src={props.icon} alt={props.iconAlt ?? "icon"} />
+      </IconContainer>
+      <Title primary={props.primary}>
         {props.title}
-      </h3>
-      <div className="info-card__text-container">
-        <p className="info-card__text">
+      </Title>
+      <TextContainer primary={props.primary}>
+        <Description primary={props.primary}>
           {props.text}
-        </p>
-      </div>
-    </div>
+        </Description>
+      </TextContainer>
+    </InfoCard>
   )
 }
