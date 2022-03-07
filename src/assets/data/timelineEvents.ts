0d86cd89cb7ef9a7f38209e31399a778140bdcf9
jmanke <jeffman879@gmail.com>
2022-03-06 20:16:14 -0800

    add esri to timeline events

--- src/assets/data/timelineEvents.ts
+++ src/assets/data/timelineEvents.ts
@@ -5,34 +5,39 @@ export const timelineEvents: ITimelineEvent[] = [
     title: 'Began Courses at UVic',
     description:
       'Attended the Univeristy of Victoria to pursue a BA in Economics',
-    date: new Date(2012, 8)
+    date: new Date(2012, 8),
   },
   {
     title: 'Added Major in Computer Science',
     description:
       'Pursued a BSc, Double Major in Computer Science and Economics',
-    date: new Date(2016, 8)
+    date: new Date(2016, 8),
   },
   {
     title: 'Junior IT Analyst at the Canada Revenue Agency',
     description:
       'Started work at the Canada Revenue Agency as a Junior I Analyst',
-    date: new Date(2016, 3)
+    date: new Date(2016, 3),
   },
   {
     title: 'Software Developer at LlamaZOO',
     description: 'Started work at LlamaZOO as a Software Developer',
-    date: new Date(2017, 8)
+    date: new Date(2017, 8),
   },
   {
     title: 'Full-Stack Developer at AlayaCare',
     description: 'Started work at Alayacare as a Full-Stack Developer',
-    date: new Date(2020, 4)
+    date: new Date(2020, 4),
   },
   {
     title: 'Completed All Courses',
     description:
       'Graduating with a BSc, Double Major in Computer Science and Economics',
-    date: new Date(2019, 11)
+    date: new Date(2019, 11),
+  },
+  {
+    title: 'Software Development Engineer at Esri',
+    description: 'Started work at Esri as a Frontend Engineer',
+    date: new Date(2021, 4),
   },
 ];
